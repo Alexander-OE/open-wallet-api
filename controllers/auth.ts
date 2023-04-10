df2a4474cb89b17689561421e51fc488304d56a4
Egbe Alexander <alexsucceeds@gmail.com>
2023-04-10 23:58:25 +0100

    import bcrypt

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from "express";
 import { UserModel } from "../models/user";
 
+
 export const getAuth = (req: Request, res: Response) => {
   res.send("Get page");
 };
 
 export const authRegister = async (req: Request, res: Response) => {
   const { email, password } = req.body;
-  console.log(req.body);
   const user = new UserModel({ email, password });
   await user.save();
   res.send("Sucessfully Registered!!!");
